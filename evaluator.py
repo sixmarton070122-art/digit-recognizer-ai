7c627f8209b9e4c105ae8c65c1827da1d5e486ee
Marton Sziksz <six.marton070122@gmail.com>
2026-09-25 21:43:59 +0200

    add: add test model and evaluation loop

--- evaluator.py
+++ evaluator.py
@@ -1,8 +1,17 @@
 import torch
+import model
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 
-test_model = torch.load("/models/test.pth")
+
+input_size=28**2
+hidden_size = 64
+num_classes = 10
+
+digit_recognizer = model.DigitClassifier(input_size=input_size, hidden_size=hidden_size, num_classes=num_classes)
+model_name = "test.pth"
+digit_recognizer.load_state_dict(torch.load(f"models/{model_name}"))
+digit_recognizer.eval()
 
 transform = transforms.Compose([
     transforms.ToTensor(),
@@ -21,6 +30,12 @@ except Exception as e:
 
 test_loader = DataLoader(test_dataset, batch_size=100)
 
-input_size=28**2
-hidden_size = 64
-num_classes = 10
+with torch.no_grad():
+    total_correct = 0
+    for i, (images, targets) in enumerate(test_loader):
+        predictions = digit_recognizer(images)
+        predicted_targets = predictions.argmax(dim=1)
+        total_correct += (predicted_targets == targets).sum().item()
+
+accuracy = total_correct / len(test_dataset)
+print(f"The model \"{model_name}\"s accuracy is: {accuracy:.3f}")
