e83b378312c2e112a27447c3ac401e09b4482b19
Marton Sziksz <six.marton070122@gmail.com>
2026-09-25 17:20:30 +0200

    fix: fix forward method

--- model.py
+++ model.py
@@ -2,15 +2,15 @@ from torch import nn
 
 class DigitClassifier(nn.Module):
     def __init__(self, input_size, hidden_size, num_classes):
-        super().__init__(self)
+        super().__init__()
         self.conv1 = nn.Conv2d(in_channels=input_size, out_channels=hidden_size, kernel_size=3)
         self.conv2 = nn.Conv2d(in_channels=hidden_size, out_channels=32, kernel_size=3)
         self.lin1 = nn.Linear(in_features=32, out_features=16)
         self.output = nn.Linear(in_features=16, out_features=num_classes)
 
     def forward(self, x):
-        x = self.conv1
-        x = self.conv2
-        x = self.lin1
-        x = self.output
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.lin1(x)
+        x = self.output(x)
         return x
