2082abff6a99d4add4a76ddbcfe43fda8f5aea3a
Marton Sziksz <six.marton070122@gmail.com>
2026-09-25 17:59:54 +0200

    add: adding extra layer and commenting

--- model.py
+++ model.py
@@ -1,15 +1,22 @@
-from torch import nn, flatten
+from torch import nn, flatten, optim
 import math
 
 class DigitClassifier(nn.Module):
     def __init__(self, input_size, hidden_size, num_classes):
         super().__init__()
-        self.conv1 = nn.Conv2d(in_channels=input_size, out_channels=64, kernel_size=3, padding=1)
-        self.conv2 = nn.Conv2d(in_channels=64, out_channels=32, kernel_size=3, padding=1)
-        self.lin1 = nn.Linear(in_features=32*28*28, out_features=hidden_size)
+        #Convolutional Layer 1
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3, padding=1)
+        #Convolutional Layer 2
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, padding=1)
+        
+        #Linear Layers
+        self.lin1 = nn.Linear(in_features=32*input_size, out_features=hidden_size)
         self.lin2 = nn.Linear(in_features=hidden_size, out_features=16)
+        
+        #Output Layer
         self.output = nn.Linear(in_features=16, out_features=num_classes)
 
+        #Activation Function
         self.relu = nn.ReLU()
 
     def forward(self, x):
